54d2985f9514379480cf8e3c8091d3700137c498
kadirlofca <kadirlofca@outlook.com>
2025-02-09 07:37:28 +0000

    check

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,12 @@
 import getAllPublishedProjects from '$lib/scripts/getAllPublishedProjects';
-import { page } from '$app/state';
 import type { LayoutServerLoad } from './$types';
 import type { Project } from '$lib/types/project.type';
 
-export const load: LayoutServerLoad = async () => {
+export const load: LayoutServerLoad = async (event) => {
 	const publishedProjects: Project[] = await getAllPublishedProjects();
-	return { publishedProjects };
+	
+	console.log(publishedProjects);
+
+	const projectsInSubdomainCategory: Project[] = publishedProjects.filter(pp => { pp.tags.includes(event.url.hostname.split('.')[0]) });
+	return { projectsInSubdomainCategory };
 };
