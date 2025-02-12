fc841703cd2064e8c0f69171f1c43de6625f1634
Kadir Lofca <kadirlofca@outlook.com>
2025-02-12 15:23:20 -0500

    Optimized profile for mobile view

--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -10,56 +10,60 @@
   const { careerData } = $props();
 </script>
 
-<div class="flex flex-col max-w-72 w-max p-4 gap-4">
-  <!-- NAME & PICTURE -->
-  <div>
+<div class="flex flex-row md:flex-col p-4 gap-4">
+  <!-- ABOUT ME -->
+  <div class="max-w-72 w-max">
     <p class="mb-2 text-xs text-secondary/50">ABOUT ME</p>
 
-    <div class="flex h-16 bg-secondary">
-      <h1 class="m-auto text-3xl font-semibold text-secondary-foreground">{Career.fullName}</h1>
+    <div class="flex flex-col gap-4">
+      <div class="flex h-16 bg-secondary">
+        <h1 class="m-auto text-3xl font-semibold text-secondary-foreground">{Career.fullName}</h1>
+      </div>
+
+      <!-- PICTURE -->
+      <img src={Headshot} alt="Kadir Lofca." class="w-max aspect-square border border-secondary" />
+
+      <!-- DESCRIPTION -->
+      <div class="p-2 bg-secondary text-sm text-pretty font-semibold text-secondary-foreground">
+        <p>{careerData.description}</p>
+      </div>
+      
+      <!-- SOCIALS -->
+      <div>
+        <div class="flex flex-wrap gap-2 w-full">
+          <a
+            href="mailto:kadirlofca@outlook.com"
+            class="w-full px-2 py-1 border border-primary-foreground text-center text-sm text-primary-foreground hover:bg-secondary hover:text-secondary-foreground"
+          >
+            kadirlofca@outlook.com
+          </a>
+          {#each Socials.socials as social}
+            <Link
+              newTab
+              href={social.href}
+              class="flex-1 border border-primary-foreground text-center text-sm text-primary-foreground hover:bg-secondary hover:text-secondary-foreground"
+            >
+              {social.title}
+            </Link>
+          {/each}
+        </div>
+      </div>
     </div>
-
-    <!-- PICTURE -->
-    <img src={Headshot} alt="Kadir Lofca." class="mt-4 w-max aspect-square border border-secondary" />  
   </div>
 
-  <!-- DESCRIPTION -->
-  <div class="p-2 bg-secondary text-sm text-pretty font-semibold text-secondary-foreground">
-    <p>{careerData.description}</p>
-  </div>
-  
-  <!-- SOCIALS -->
-  <div>
-    <div class="flex flex-wrap gap-2 w-full">
-      <a
-        href="mailto:kadirlofca@outlook.com"
-        class="w-full px-2 py-1 border border-primary-foreground text-center text-sm text-primary-foreground hover:bg-secondary hover:text-secondary-foreground"
-      >
-        kadirlofca@outlook.com
-      </a>
-      {#each Socials.socials as social}
-        <Link
-          newTab
-          href={social.href}
-          class="flex-1 border border-primary-foreground text-center text-sm text-primary-foreground hover:bg-secondary hover:text-secondary-foreground"
-        >
-          {social.title}
-        </Link>
-      {/each}
-    </div>
-  </div>
-
-  <!-- SKILLS -->
-  <div class="mt-2">
+  <!-- SKILL DISTRIBUTION -->
+  <div class="md:mt-2 w-full">
     <p class="mb-2 text-xs text-secondary/50">SKILL DISTRIBUTION</p>
     <div class="flex flex-col gap-2">
       {#each getSkillDistribution() as skill}
         {@render skillBar(skill, 60)}
       {/each}
     </div>
-  </div>
 
-  <Credits />
+    <div class="mt-2 text-center">
+      <Credits />
+    </div>
+  </div>
 </div>
 
 {#snippet skillBar(title: string, percentage: number)}
